fdac929f8d8c5d0184ca656ca4e32a2c0614c38e
jonathan gregis <jon.gregis@gmail.com>
2020-01-10 13:27:05 -0600

    server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const config = require("config");
 const app = express();
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 // Bodyparser Middleware
 app.use(express.json());
 app.use(cors());
@@ -33,13 +34,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "./index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
-
-const PORT = process.env.PORT || 5000;
-// app.listen(3000, function() {
-//   console.log("CORS-enabled web server listening on port 3001");
-// });
 
 app.listen(PORT, () => `Server running on port ${PORT}`);
