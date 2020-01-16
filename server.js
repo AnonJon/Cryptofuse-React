02dcf3eacc95d2c4a2a2543ea176e711ec0c7c6e
jonathan gregis <jon.gregis@gmail.com>
2020-01-16 11:23:41 -0600

    server

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => `Server running on port ${PORT}`);
